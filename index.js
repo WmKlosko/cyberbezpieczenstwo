34423f8a735804be006a6310a3c9731fc6f68fe2
Wojciech Klosko <wmklosko@gmail.com>
2022-11-07 20:37:58 +0100

    drugi commit

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ function login() {
     const username = document.getElementById("user").value;
     const password = document.getElementById("password").value;
 
+    validateUser(username, password);
+
     const authenticated = true;
 
     if (authenticated === true) {
         window.location.href = './home.html';
     }
+}
+
+function validateUser(username, password)
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", "./fakeapi/users.json", false);
+    request.overrideMimeType("application/json");
+    request.send(null);
+    var usersJson = JSON.parse(request.responseText);
+    console.log(usersJson);
+
+    // $.getJSON("./fakeapi/users.json", function(json) {
+    //     console.log(json);
+    // })
 }
